8880650019510a9a86f8ce44348ecd835bce6e9d
bagal-co <withbob2@gmail.com>
2020-10-24 07:35:42 +1000

    trying to fix the error in the cardList component

--- src/components/card_list/card_list.component.jsx
+++ src/components/card_list/card_list.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Card } from '../card/card.component';
 import './card_list.style.css';
 
 export const CardList = (props) =>{
-  <div className="card_list_wrapper">
-    {props.monsters.map(monster =>(
-      <h1 key={monster.id}>{monster.name}</h1>
-    ))}
+  <div className="cardList_wrapper">
+    {
+      props.monsters.map((monster) =>{
+      < Card key={monster.id} monsters={monsters} />
+      })
+    }
   </div>
 }
 
- 
+//<h2 key={monster.id}>{monster.name}</h2>
